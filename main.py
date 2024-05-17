198ed3c805ef65c8ab4c19cb8f2e6e4900124053
kirici <74684021+kirici@users.noreply.github.com>
2024-05-17 18:32:39 +0200

    feat: sample widgets added

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+"""
+A sample streamlit application.
+"""
+import numpy as np
+import pandas as pd
 import streamlit as st
-# import pandas as pd
 
-st.title('Hello World')
+
+def init_data() -> pd.DataFrame:
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+    return chart_data
+
+
+def render(df=pd.DataFrame):
+    st.title('Hello World')
+    st.line_chart(df)
+
+    x = st.slider('x')
+    st.write(x, 'squared is', x * x)
+
+
+d = init_data()
+render(df=d)
