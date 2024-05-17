a9a969b5362c970b264f45198688122b624abbc6
kirici <74684021+kirici@users.noreply.github.com>
2024-05-17 19:36:44 +0200

    feat: espresso order added

--- pages/2_Orders.py
+++ pages/2_Orders.py
@@ -1,3 +1,5 @@
+import time
+
 import streamlit as st
 
 st.set_page_config(
@@ -14,4 +16,15 @@ def page_dressing():
     st.sidebar.markdown("# Orders")
 
 
+def coffee_order():
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+    for i in range(100):
+        latest_iteration.text(f'{i+1} ml coffee extracted')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+
 page_dressing()
+if st.button("Espresso", type="primary"):
+    coffee_order()
